ba5a03fadccd99b91c9c1f14d9dccb9521493ce5
Pavel Ermakov <petechka_petrov_201@mail.ru>
2021-11-18 01:52:44 +0300

    dev-5-ermakov Добавил компонент TopBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import {Button} from "./components/common/Button"
+import {TopBar} from "./components/TopBar/TopBar";
 
 function App() {
     return <div>
+        <TopBar/>
         <Button text={'Первый компонент!'} state={"default"} onClick={() => console.log('onClick')}/>
     </div>
 }
