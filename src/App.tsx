e71024105d28ff3e5ad1fa092977b84fec5ff2cc
beta442 <grandoblivioniv@gmail.com>
2021-11-18 20:53:04 +0300

    фикс: стили

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
 import "./App.css";
-import {Button} from "./components/common/Button"
-import {TopBar} from "./components/TopBar/TopBar";
+import { TopBar } from "./components/TopBar/TopBar";
 
 function App() {
-    return <div>
-        <TopBar/>
-        <Button text={'Первый компонент!'} state={"default"} onClick={() => console.log('onClick')}/>
+  return <PresentationEditor />;
+}
+
+function PresentationEditor(): JSX.Element {
+  return (
+    <div className="editor">
+      <TopBar />
+      <Center />
+      <Bottom />
     </div>
+  );
+}
+
+function Center(): JSX.Element {
+  return (
+    <div className="center">
+      <SlideList />
+      <Slide />
+    </div>
+  );
+}
+
+function Bottom(): JSX.Element {
+  return (
+    <div className="bottom">
+      <SlideListTool />
+      <SlideInfo />
+    </div>
+  );
+}
+
+function SlideList(): JSX.Element {
+  return <div className="slides-list">slidelist</div>;
+}
+
+function Slide(): JSX.Element {
+  return <div className="slide">Slide</div>;
+}
+
+function SlideListTool(): JSX.Element {
+  return <div className="slides-list-tools">tools</div>;
+}
+
+function SlideInfo(): JSX.Element {
+  return <div className="slide-info">slideInfo</div>;
 }
 
 export default App;
--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,32 +1,26 @@
-import {Button} from "../common/Button";
-import styles from './TopBar.module.css'
+import { Button } from "../common/Button/Button";
+import styles from "./TopBar.module.css";
 
 function TopBar() {
-    const mockData = [
+    const mockData: string[] = [
         'Файл',
         'Вставка',
         'Показ слайдов',
-    ]
-    const buttonList = mockData.map(text => {
-        return <Button text={text} state={"default"} onClick={() => console.log('hello')}/>
-    })
+    ];
 
-    return <div
-        className={styles['top-bar']}
-    >
-        <div
-            className={styles['top-bar__text']}
-        >
-            <span>Презентация 1</span>
+    const buttonList: JSX.Element[] = mockData.map(text => {
+        return <Button text={text} state={"default"} onClick={() => console.log('hello')} />
+    });
+
+    return (
+        <div className={styles['top-bar']}>
+            <div className={styles['top-bar__button-list']}>
+                {buttonList}
+            </div>
         </div>
-        <div
-            className={styles['top-bar__button-list']}
-        >
-            {buttonList}
-        </div>
-    </div>
+    );
 }
 
 export {
-    TopBar,
+    TopBar
 }
