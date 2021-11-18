e00092b160fe65946cce235776345c509770916a
beta442 <grandoblivioniv@gmail.com>
2021-11-18 23:14:26 +0300

    фича: стиль dropdown-menu

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,4 +1,5 @@
 import { Button } from "../common/Button/Button";
+import { DropdownButton } from "../common/DropdownButton/DropdownButton";
 import styles from "./TopBar.module.css";
 
 function TopBar() {
@@ -12,9 +13,14 @@ function TopBar() {
         return <Button text={text} state={"default"} onClick={() => console.log('hello')} />
     });
 
+    const dropDownMenu: Map<string, boolean> = new Map();
+    dropDownMenu.set("Open", false);
+    dropDownMenu.set("Fuck", true);
+
     return (
         <div className={styles['top-bar']}>
             <div className={styles['top-bar__button-list']}>
+            <DropdownButton title="File" state="default" menu={dropDownMenu} onClick={() => console.log('stop')} />
                 {buttonList}
             </div>
         </div>
--- src/components/common/DropdownButton/DropdownButton.tsx
+++ src/components/common/DropdownButton/DropdownButton.tsx
@@ -1,9 +1,34 @@
-import style from "./DropdownButton.module.css";
+import styles from "./DropdownButton.module.css";
 
-export function DropdownButton(): JSX.Element {
+export function DropdownButton(props: {
+    title: string;
+    state: 'disabled' | 'default';
+    menu: Map<string, boolean>;
+    onClick: () => void;
+}): JSX.Element {
+    const {
+        title,
+        state,
+        menu,
+        onClick
+    } = props;
+
+    const menuElements: JSX.Element[] = [];
+    menu.forEach((hasSubMenu, title) => {
+        const element: JSX.Element = (hasSubMenu) 
+            ? <div className={styles["clickable-subbuton"]}>{title}</div>
+            : <div className={styles.subbuton}>{title}</div>;
+        menuElements.push(element);
+    });
+
     return (
-        <div className={style.button}>
-            Hello
+        <div className={styles.dropdown}>
+            <button className={styles.button}>
+                {title}
+            </button>
+            <div className={styles["dropdown-menu"]}>
+              {menuElements}
+            </div>
         </div>
-    );    
+    );
 }
