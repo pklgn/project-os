99145de142a5be34e26b6192e020366891c9aa01
beta442 <grandoblivioniv@gmail.com>
2021-11-19 23:50:19 +0300

    рефактор: компонент кнопки

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -3,30 +3,27 @@ import styles from "./Button.module.css";
 
 type ButtonProps = {
     title: string;
-    isInDropDown: boolean;
-    atDropDownMenuProps: {
-        hasTriangleAndSubMenu: boolean;
-        isBlockEnd: boolean;
-    };
-    hotkeyInfo: string;
+    elements: {
+        triangle: JSX.Element | undefined;
+        hotkeyInfo: string;
+        icon: JSX.Element | undefined;
+    }
 }
 
 export function Button(props: ButtonProps = {
     title: "",
-    isInDropDown: false,
-    atDropDownMenuProps: {
-        hasTriangleAndSubMenu: false,
-        isBlockEnd: false
-    },
-    hotkeyInfo: "",
+    elements: {
+        triangle: undefined,
+        hotkeyInfo: "",
+        icon: undefined
+    }
 }): JSX.Element {
-    const { title, isInDropDown } = props;
-    const hasTriangleAndSubMenu: boolean = props.atDropDownMenuProps.hasTriangleAndSubMenu;
-    const isBlockEnd: boolean = props.atDropDownMenuProps.isBlockEnd;
-    const hotkeyInfo: string = props.hotkeyInfo;
+    const { title, elements } = props;
+    const triangle: JSX.Element | undefined = elements.triangle;
+    const icon: JSX.Element | undefined = elements.icon;
+    const hotkeyInfo: string = elements.hotkeyInfo;
 
     const [buttonStyle, setButtonStyle] = useState(styles["button-default"]);
-    const [buttonInDropDownMenuStyle, setbuttonInDropDownMenuStyle] = useState(styles["dropdown-button-content"]);
 
     const handlerMouseDown = (_: BaseSyntheticEvent) => {
         setButtonStyle(styles["button-on"]);
@@ -42,30 +39,8 @@ export function Button(props: ButtonProps = {
     const handlerBlur = (_: BaseSyntheticEvent) => {
     }
 
-    const button: JSX.Element = (isInDropDown)
+    const button: JSX.Element = (triangle === undefined && icon === undefined)
         ? <button
-            className={styles["dropdown-button"]}
-            onMouseDown={handlerMouseDown}
-            onMouseUp={handlerMouseUp}
-            onFocus={handlerMouseFocus}
-            onBlur={handlerBlur}
-            id="button-in-dropdown"
-        >
-            <div
-                className={buttonInDropDownMenuStyle}
-            >
-                {title}
-                {(hasTriangleAndSubMenu)
-                    ? <div className={styles.triangle}></div>
-                    : ''
-                }
-                {(hotkeyInfo)
-                  ? <div>{hotkeyInfo}</div> 
-                  : ''
-                }
-            </div>
-        </button>
-        : <button
             className={buttonStyle}
             onMouseDown={handlerMouseDown}
             onMouseUp={handlerMouseUp}
@@ -74,15 +49,29 @@ export function Button(props: ButtonProps = {
             id="button"
         >
             {title}
+        </button>
+        : <button
+            className={styles["dropdown-button"]}
+            onMouseDown={handlerMouseDown}
+            onMouseUp={handlerMouseUp}
+            onFocus={handlerMouseFocus}
+            onBlur={handlerBlur}
+            id="button-in-dropdown"
+        >
+            <div
+                className={styles["dropdown-button-content"]}
+            >
+                {(triangle === undefined)
+                  ? icon
+                  : ''
+                }
+                {title}
+                {(icon === undefined)
+                    ? triangle
+                    : ''
+                }
+            </div>
         </button>;
 
-    return (
-        <div className={styles.button}>
-            {button}
-            {(isBlockEnd)
-                ? <div className={styles["block-end-line"]}></div>
-                : ''
-            }
-        </div>
-    );
+    return button;
 }
