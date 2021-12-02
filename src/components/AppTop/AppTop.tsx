56a332e200ea97123147bcea9a44119a0a8e0c4c
beta442 <grandoblivioniv@gmail.com>
2021-12-02 17:49:16 +0300

    стиль: топбар

--- src/components/AppTop/AppTop.tsx
+++ src/components/AppTop/AppTop.tsx
@@ -1,11 +1,14 @@
 import { BaseSyntheticEvent, useEffect, useState } from 'react';
+
+import { AdaptiveInputField } from '../common/AdaptiveInputField/AdaptiveInputField';
+import { AppLogoSVG } from '../common/icons/AppLogo';
+import { ToolBar } from '../common/ToolBar/ToolBar';
+
 import { connect } from 'react-redux';
 import { Editor } from '../../model/types';
 import { changePresentationTitle } from '../../redux/actions';
 import { store } from '../../redux/store';
-import { AdaptiveInputField } from '../common/AdaptiveInputField/AdaptiveInputField';
-import { AppLogoSVG } from '../common/icons/AppLogo';
-import { ToolBar } from '../common/ToolBar/ToolBar';
+
 import styles from './AppTop.module.css';
 
 export function AppTop(): JSX.Element {
@@ -20,25 +23,16 @@ export function AppTop(): JSX.Element {
         mapDispatchToProps(store.dispatch, event.target.value).changePresentationTitle();
         setPresentationTitle(store.getState().presentation.name);
     }
-    const onClickHandler = (_: BaseSyntheticEvent) => {
-        console.log(store.getState().presentation.name);
-    }
 
     return (
         <div
             className={styles['top-bar']}
-            onClick={onClickHandler}
         >
             <div className={styles["logo-container"]}>
                 <AppLogoSVG width={55} height={55} type={'default'} />
             </div>
-            <input
-                className={styles.input}
-                type="text"
-                value={presentationTitle}
-                onChange={onChangeHandler}
-            />
             <ToolBar />
+            <AdaptiveInputField value={presentationTitle} onChange={onChangeHandler} />
         </div>
     );
 }
