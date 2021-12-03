9f19cd71d6422f609a4ea1f18e8636b65aabeab3
Kelrogg <Taltolka@yandex.ru>
2021-12-03 13:47:12 +0300

    Простая анимация готова. Никаких TransitionGroup, никаких библиотек. Сойдёт

--- src/components/AppFooter/ElementListTool/ElementListTool.tsx
+++ src/components/AppFooter/ElementListTool/ElementListTool.tsx
@@ -7,16 +7,18 @@ import { Opacity } from "../../common/icons/Opacity/Opacity"
 import { Delete } from "../../common/icons/Delete/Delete";
 import { Fullscreen } from "../../common/icons/Fullscreen/Fullscreen";
 import { VerticalLine } from "../../common/VerticalLine/VerticalLine";
-import { LayerBackward } from "../../common/icons/LayerBackward/LayerBackward";
-import { LayerForward } from "../../common/icons/LayerForward/LayerForward";
 
-export function ElementListTool(): JSX.Element {
+type ElementListToolProps = {
+    foo: Function | undefined
+}
+
+export function ElementListTool(props: ElementListToolProps): JSX.Element {
     return <div className={styles["element-tools"]}>
         <Button text="" state="active" contentType="icon" content={{hotkeyInfo: "", icon: <Undo />}} foo={undefined}/>
         <VerticalLine />
         <Button text="" state="active" contentType="icon" content={{hotkeyInfo: "", icon: <Redo />}} foo={undefined}/>
         <VerticalLine />
-        <Button text="" state="active" contentType="icon" content={{hotkeyInfo: "", icon: <Reorder />}} foo={undefined}/>
+        <Button text="" state="active" contentType="icon" content={{hotkeyInfo: "", icon: <Reorder />}} foo={props.foo}/>
         <VerticalLine />
         <Button text="" state="active" contentType="icon" content={{hotkeyInfo: "", icon: <Opacity />}} foo={undefined}/>
         <VerticalLine />
--- src/components/PresentationEditor/PresentationEditor.tsx
+++ src/components/PresentationEditor/PresentationEditor.tsx
@@ -1,18 +1,26 @@
+import { useState } from "react";
 import { TopBar } from "../AppTop/TopBar";
 import styles from "./PresentationEditor.module.css";
 import { ElementListTool } from "../AppFooter/ElementListTool/ElementListTool";
+import { ReorderListTool } from "../AppFooter/ReorderList Tool/ReorderListTool";
 import { SlideListTool } from "../AppFooter/SlideListTool/SlideListTool";
 import { SlideList } from "../AppContent/SlideList/SlideList";
 import { Slide } from "../AppContent/Slide/Slide";
 
 export function PresentationEditor(): JSX.Element {
+    const [menuSwitcher, setMenuSwitcher] = useState(true);
+    const handleToggleView = () => setMenuSwitcher(!menuSwitcher);
     return (
         <div className={styles.editor}>
             <TopBar />
             <SlideList />
             <Slide />
             <SlideListTool />
-            <ElementListTool />
+            {
+                menuSwitcher
+                ? <ElementListTool foo={handleToggleView}/>
+                : <ReorderListTool foo={handleToggleView}/>
+            }
         </div>
     );
 }
