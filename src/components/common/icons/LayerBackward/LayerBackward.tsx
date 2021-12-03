9cf7b127fd63ae59f62517faa783b3c80c002628
Kelrogg <Taltolka@yandex.ru>
2021-12-03 13:57:43 +0300

    Фиксы иконок

--- src/components/common/icons/LayerBackward/LayerBackward.tsx
+++ src/components/common/icons/LayerBackward/LayerBackward.tsx
@@ -2,7 +2,7 @@ export function LayerBackward(): JSX.Element {
     return (
         <svg enable-background="new 0 0 24 24" height="24px" 
             viewBox="0 0 24 24" width="24px" fill="#208EB2">
-            <rect fill="#208EB2" height="24" width="24"/>
+            <rect fill="none" height="24" width="24"/>
                 <path d="M3,11c0,2.45,1.76,4.47,4.08,4.91l-1.49-1.49L7,13l4,4.01L7,
                 21l-1.41-1.41l1.58-1.58l0-0.06C3.7,17.54,1,14.58,1,11 c0-3.87,3.13-7,7-7h3v2H8C5.24,6,3,8.24,3,
                 11z"/>
