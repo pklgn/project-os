7ce2408ef0ccd8628dbbcc7397db140bc95303d5
beta442 <grandoblivioniv@gmail.com>
2021-12-23 20:19:26 +0300

    фича: редусер презентации

--- src/redux/reducers/rootReducer.tsx
+++ src/redux/reducers/rootReducer.tsx
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import elementReducers from "./elementReducers"
+import { presentationReducers } from "./presentationReducers";
 
 
 const reducers = combineReducers({
-    element: elementReducers
+    element: elementReducers,
+    presentation: presentationReducers
 })
 
 export default reducers
