347343937f3952759c96c062f9e5f15d299e9702
Kelrogg <Taltolka@yandex.ru>
2021-12-23 22:33:58 +0300

    SlideListTool - добавлены

--- src/components/AppFooter/SlideListTool/SlideListTool.tsx
+++ src/components/AppFooter/SlideListTool/SlideListTool.tsx
@@ -1,5 +1,17 @@
 import styles from "./SlideListTool.module.css";
+import { Button } from "../../common/Button/Button"
+import { VerticalLine } from "../../common/VerticalLine/VerticalLine";
+import { AddSlide } from "../../common/icons/AddSlide/AddSlide";
+import { RemoveSlide } from "../../common/icons/RemoveSlide/RemoveSlide";
 
-export function SlideListTool(): JSX.Element {
-    return <div className={styles["slides-list-tools"]}>tools</div>;
+type SlideListToolProps = {
+    foo: () => void | undefined
+}
+
+export function SlideListTool(props: SlideListToolProps): JSX.Element {
+    return <div className={styles["slides-list-tools"]}>
+        <Button text="Add" state="disabled" contentType="icon" content={{hotkeyInfo: "", icon: <AddSlide />}} foo={props.foo}/>
+        <VerticalLine />
+        <Button text="Remove" state="disabled" contentType="icon" content={{hotkeyInfo: "", icon: <RemoveSlide />}} foo={props.foo}/>        
+    </div>;
 }
