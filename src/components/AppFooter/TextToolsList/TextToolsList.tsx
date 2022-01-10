c112bc0c30dfdbb3b812cec579ddef9a904a24d2
Kelrogg <Taltolka@yandex.ru>
2022-01-10 17:18:29 +0300

    + TextListTool/AddText

--- src/components/AppFooter/TextToolsList/TextToolsList.tsx
+++ src/components/AppFooter/TextToolsList/TextToolsList.tsx
@@ -1,22 +1,18 @@
-import styles from './ElementListTool.module.css';
+import styles from './TextToolsList.module.css';
 
 import { LocaleContext, LocaleContextType } from '../../../App';
 import { useContext } from 'react';
 
 import { Button } from '../../common/Button/Button';
-import { Delete } from '../../common/icons/Delete/Delete';
-import { Fullscreen } from '../../common/icons/Fullscreen/Fullscreen';
-import { Opacity } from '../../common/icons/Opacity/Opacity';
-import { Redo } from '../../common/icons/Redo/Redo';
-import { Reorder } from '../../common/icons/Reorder/Reorder';
 import { VerticalLine } from '../../common/VerticalLine/VerticalLine';
-import { Undo } from '../../common/icons/Undo/Undo';
 import { listName } from '../../PresentationEditor/PresentationEditor';
 
 import { bindActionCreators } from 'redux';
 import { useDispatch } from 'react-redux';
 import { undoModelAction, redoModelAction } from '../../../redux/action-creators/editorActionCreators';
-import { TextTools } from '../../common/icons/TextTools/TextTools';
+import { addText } from '../../../redux/action-creators/textActionCreators';
+import { AddText } from '../../common/icons/AddText/AddText';
+import { Clear } from '../../common/icons/Cancel/Clear';
 
 type TextToolsListProps = {
     foo: (listName: listName) => void | undefined;
@@ -25,11 +21,12 @@ type TextToolsListProps = {
 export function TextToolsList(props: TextToolsListProps): JSX.Element {
     const localeContext: LocaleContextType = useContext(LocaleContext);
     
-    const textToolsListButton = () => props.foo(listName.TEXT_TOOLS_LIST_BUTTON)
+    const elementListToolButton = () => props.foo(listName.ELEMENT_LIST)
 
     const dispatch = useDispatch();
     const dispatchSetPreviousModelStateAction = bindActionCreators(undoModelAction, dispatch);
     const dispatchTurnBackModelStateAction = bindActionCreators(redoModelAction, dispatch);
+    const dispatchAddTextAction = bindActionCreators(addText, dispatch);
 
     const undoPressButtonHandler = () => {
         dispatchSetPreviousModelStateAction();
@@ -39,6 +36,12 @@ export function TextToolsList(props: TextToolsListProps): JSX.Element {
         dispatchTurnBackModelStateAction();
     };
 
+    const addTextHandler = () => {
+        dispatchAddTextAction({
+            x: 20,
+            y: 30
+        });
+    }
     document.addEventListener('keydown', function (event) {
         if (event.code == 'KeyZ' && (event.ctrlKey || event.metaKey)) {
             undoPressButtonHandler();
@@ -50,7 +53,23 @@ export function TextToolsList(props: TextToolsListProps): JSX.Element {
 
     return (
         <div className={styles['element-tools']}>
-            
+            <Button
+                text={localeContext.locale.localization.add_word}
+                state="disabled"
+                shouldStopPropagation={false}
+                contentType="icon"
+                content={{ hotkeyInfo: '', icon: <AddText /> }}
+                foo={addTextHandler}
+            />
+            <VerticalLine />
+            <Button
+                text={localeContext.locale.localization.delete_word}
+                state="disabled"
+                shouldStopPropagation={false}
+                contentType="icon"
+                content={{ hotkeyInfo: '', icon: <Clear /> }}
+                foo={elementListToolButton}
+            />
         </div>
     );
 }
