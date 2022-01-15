329b53e17a228cc2e8412b8f334541bf0d86b7b5
beta442 <grandoblivioniv@gmail.com>
2022-01-15 20:00:03 +0300

    фича: кнопка

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -2,45 +2,76 @@ import styles from './Button.module.css';
 import CSS from 'csstype';
 
 import { joinClassNames } from '../../utils/joinClassNames';
-import { BaseSyntheticEvent } from 'react';
+import { BaseSyntheticEvent, MouseEvent, useEffect, useRef, useState } from 'react';
 
-type ButtonState = 'pressed' | 'active' | 'disabled' | 'regardless';
+type ButtonState = 'pressed' | 'active' | 'disabled' | 'independently';
 type ButtonType = 'in-list' | 'default';
 
 export type ButtonProps = {
     type: ButtonType;
     state: ButtonState;
     id: string;
-    shouldStopPropagation: boolean;
     text: string | undefined;
     optionalText: string | undefined;
     iconLeft: JSX.Element | undefined;
     iconRight: JSX.Element | undefined;
     cssMix: CSS.Properties | undefined;
-    func: () => void;
+    onClick: (_: BaseSyntheticEvent) => void;
 };
 
 export function Button(props: ButtonProps) {
-    const { type, state, id, shouldStopPropagation, text, optionalText, iconLeft, iconRight, cssMix, func } = props;
+    const { type, state, id, text, optionalText, iconLeft, iconRight, cssMix, onClick } = props;
+    const buttonRef = useRef<HTMLButtonElement>(null);
 
     const disabled = state === 'disabled';
 
-    const classes = joinClassNames(['button', type, state ?? ''])
-        .trim()
-        .split(' ');
-    const onMouseDownHandler = (event: BaseSyntheticEvent) => {
-        if (shouldStopPropagation) {
-            event.stopPropagation();
+    const [classes, setClasses] = useState(
+        joinClassNames(['button', type ?? '', state ?? ''])
+            .trim()
+            .split(' '),
+    );
+
+    const onMouseDownHandler = () => {
+        console.log('mousedown');
+        // if (state === 'independently') {
+        //     setClasses(
+        //         joinClassNames(['button', type ?? '', 'active'])
+        //             .trim()
+        //             .split(' '),
+        //     );
+        // }
+    };
+
+    const onEnter = (event: MouseEvent) => {
+        if (event.buttons === 1) {
+            console.log('entered and pressed');
         }
     };
 
-    const onClick = (event: BaseSyntheticEvent) => {
-        func();
+    const onMouseUpHandler = () => {
+        if (state === 'independently') {
+            setClasses(
+                joinClassNames(['button', type ?? '', 'disabled'])
+                    .trim()
+                    .split(' '),
+            );
+        }
     };
+
+    useEffect(() => {
+        document.addEventListener('mouseup', onMouseUpHandler);
+
+        return () => {
+            document.removeEventListener('mouseup', onMouseUpHandler);
+        };
+    }, [onMouseUpHandler]);
 
     return (
         <button
+            ref={buttonRef}
             className={`${styles[classes[0]] ?? ''} ${styles[classes[1]] ?? ''} ${styles[classes[2]] ?? ''}`}
+            onMouseDown={onMouseDownHandler}
+            onMouseEnter={onEnter}
             onClick={onClick}
             style={cssMix}
             id={id}
